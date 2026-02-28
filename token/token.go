484f42b7c82ca510d496becf77fe55b74dbeb758
Alan Valdevenito <avaldevenito@fi.uba.ar>
2026-02-28 18:06:09 -0300

    add token constants for various types and operators

--- token/token.go
+++ token/token.go
@@ -6,3 +6,29 @@ type Token struct {
 	Type    TokenType
 	Literal string
 }
+
+const (
+	ILLEGAL = "ILLEGAL" // Special type. Signifies a token/character we don't know about.
+	EOF     = "EOF" // Special type. Signifies we've reached the end of the file/input.
+
+	// Identifiers + literals
+	IDENT  = "IDENT"  // add, foobar, x, y, ...
+	INT    = "INT"    // 1343456
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN   = "("
+	RPAREN   = ")"
+	LBRACE   = "{"
+	RBRACE   = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
