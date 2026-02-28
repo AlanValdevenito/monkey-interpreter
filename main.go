263414bde67588003d677591f3a16cfa137061e2
Alan Valdevenito <avaldevenito@fi.uba.ar>
2026-02-28 20:31:44 -0300

    refactor main function to include user greeting and start REPL

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/user"
 
-func helloWorld() {
-	fmt.Println("Hello, World!")
-}
+	"github.com/AlanValdevenito/monkey-interpreter/repl"
+)
 
 func main() {
-	helloWorld()
+	user, err := user.Current()
+	if err != nil {
+		fmt.Println("Error fetching user information:", err)
+		return
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
